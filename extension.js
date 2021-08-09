0db76e7bf486f24903678e0a56d16944dde1e5d0
homomodern <homomodern@gmail.com>
2021-08-09 18:00:37 +0600

    Totally rewritten findBestQaulty function and some other minor changes

--- extension.js
+++ extension.js
@@ -1,10 +1,10 @@
 'use strict'
 
 // iconDownload made by https://www.flaticon.com/authors/freepik
-const iconDownload = '<svg width="24" height="24" viewBox="0 0 512 512" fill="#262626"><g><g><path d="M472,313v139c0,11.028-8.972,20-20,20H60c-11.028,0-20-8.972-20-20V313H0v139c0,33.084,26.916,60,60,60h392 c33.084,0,60-26.916,60-60V313H472z"></path></g></g><g><g><polygon points="352,235.716 276,311.716 276,0 236,0 236,311.716 160,235.716 131.716,264 256,388.284 380.284,264"></polygon></g></g></svg>'
+const iconDownload = `<svg width="24" height="24" viewBox="0 0 512 512" fill="#262626"><g><g><path d="M472,313v139c0,11.028-8.972,20-20,20H60c-11.028,0-20-8.972-20-20V313H0v139c0,33.084,26.916,60,60,60h392 c33.084,0,60-26.916,60-60V313H472z"></path></g></g><g><g><polygon points="352,235.716 276,311.716 276,0 236,0 236,311.716 160,235.716 131.716,264 256,388.284 380.284,264"></polygon></g></g></svg>`
 // iconNewtab made by https://www.flaticon.com/authors/those-icons
-const iconNewtab = '<svg width="24" height="24" viewBox="0 0 482.239 482.239" fill="#262626"><path d="m465.016 0h-344.456c-9.52 0-17.223 7.703-17.223 17.223v86.114h-86.114c-9.52 0-17.223 7.703-17.223 17.223v344.456c0 9.52 7.703 17.223 17.223 17.223h344.456c9.52 0 17.223-7.703 17.223-17.223v-86.114h86.114c9.52 0 17.223-7.703 17.223-17.223v-344.456c0-9.52-7.703-17.223-17.223-17.223zm-120.56 447.793h-310.01v-310.01h310.011v310.01zm103.337-103.337h-68.891v-223.896c0-9.52-7.703-17.223-17.223-17.223h-223.896v-68.891h310.011v310.01z"></path></svg>'
-const quality = ['640w', '750w', '1080w']
+const iconNewtab = `<svg width="24" height="24" viewBox="0 0 482.239 482.239" fill="#262626"><path d="m465.016 0h-344.456c-9.52 0-17.223 7.703-17.223 17.223v86.114h-86.114c-9.52 0-17.223 7.703-17.223 17.223v344.456c0 9.52 7.703 17.223 17.223 17.223h344.456c9.52 0 17.223-7.703 17.223-17.223v-86.114h86.114c9.52 0 17.223-7.703 17.223-17.223v-344.456c0-9.52-7.703-17.223-17.223-17.223zm-120.56 447.793h-310.01v-310.01h310.011v310.01zm103.337-103.337h-68.891v-223.896c0-9.52-7.703-17.223-17.223-17.223h-223.896v-68.891h310.011v310.01z"></path></svg>`
+//const quality = ['640w', '750w', '1080w']
 let currentUrl = document.location.href
 let updating = false
 
@@ -25,18 +25,13 @@ function init (times) {
 }
 
 function addButton () {
-  // get panel
+  // get action panel
   document.querySelectorAll('section.ltpMr.Slqrh:not(.section-set)').forEach(panel => {
     panel.classList.add('section-set')
     // button 1: download
-    // firefox doesn't support direct download function.
-    // const isFirefox = typeof InstallTrigger !== 'undefined';
-    // if (!isFirefox)
     panel.lastElementChild.before(createButton('download-set', iconDownload))
-    // createButton(panel, 'download-set', iconDownload)
     // button 2: new tab
     panel.lastElementChild.before(createButton('newtab-set', iconNewtab))
-    // createButton(panel, 'newtab-set', iconNewtab)
   })
 }
 
@@ -49,10 +44,10 @@ function createButton (myClass, icon) {
 
   switch (myClass) {
     case 'download-set':
-      button.addEventListener('pointerdown', download)
+      button.addEventListener('pointerdown', downloadImage)
       break
     case 'newtab-set':
-      button.addEventListener('pointerdown', imageNewTab)
+      button.addEventListener('pointerdown', newTabImage)
       break
   }
 
@@ -72,7 +67,7 @@ function checkSort () {
     })
 }
 
-function imageNewTab () {
+function newTabImage () {
   const tab = window.open(getSourceLink.call(this), '_blank')
   tab.focus()
 }
@@ -84,7 +79,7 @@ function getSourceLink () {
   const file = single
     ? parent.querySelector('video') || parent.querySelector('img')
     : detectIndex(parent, parent.querySelectorAll('li.Ckrof'))
-  const link = file.srcset ? qualityPhoto(file.srcset) : file.src
+  const link = file.srcset ? findBestQuality(file.srcset) : file.src
   return link
 }
 
@@ -100,31 +95,28 @@ function detectIndex (parent, files) {
   return file.querySelector('video') || file.querySelector('img')
 }
 
-function qualityPhoto (srcset) {
-  const srcs = srcset.split(/ |,/)
-  for (let i = srcs.length - 1; i > 0; i--) {
-    for (let j = quality.length - 1; j > 0; j--) {
-      if (srcs[i] === quality[j]) {
-        return srcs[i - 1]
-      }
-    }
+function findBestQuality (srcset) {
+  const srcs = srcset.split(/\s|,\s?/).reverse()
+  const srcsObj = {}
+  while(srcs.length) {
+    Object.assign(srcsObj, Object.fromEntries([srcs.splice(0,2)]))
   }
-  console.log('Error: there is no any quality of photo.')
-  return null
+  return srcsObj[Math.max(...Object.keys(srcsObj)
+    .map(key => Number.parseInt(key))) + 'w'] || alert('No data available')
 }
 
-function download () {
+function downloadImage () {
   const article = this.closest('article')
   const link = getSourceLink.call(this)
-  fetch(link).then(t => {
-    return t.blob().then(b => {
-      const a = document.createElement('a')
-      const name = `${getUser(article)}_${getTime(article)}${getIndex(article)}`
-      a.href = URL.createObjectURL(b)
-      a.setAttribute('download', name)
-      a.click()
-    })
-  })
+  fetch(link)
+  .then(res => res.blob())
+  .then(blob => {
+    const a = document.createElement('a')
+    const name = `${getUser(article)}_${getTime(article)}${getIndex(article)}`
+    a.href = URL.createObjectURL(blob)
+    a.download = name
+    a.click()
+  }).catch(err => console.log(err.message))
 }
 
 function getUser (article) {
@@ -139,10 +131,10 @@ function getTime (article) {
 function getIndex (article) {
   const index = article.querySelectorAll('.Yi5aA')
   if (index.length > 1) {
-    // multiple
+    // photo set
     return `-${[...index].findIndex(index => index.classList.contains('XCodT')) + 1}`
   } else {
-    // single
+    // single photo
     return ''
   }
 }
